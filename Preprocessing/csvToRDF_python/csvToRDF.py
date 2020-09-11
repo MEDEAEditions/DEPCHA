946758d76747e3c11ecdddd6bc1b9d4231ef5b8a
chpollin <christopher.pollin@uni-graz.at>
2020-09-11 09:43:03 +0200

    update csv to rdf (marprof)

--- Preprocessing/csvToRDF_python/csvToRDF.py
+++ Preprocessing/csvToRDF_python/csvToRDF.py
@@ -3,18 +3,21 @@ from rdflib import Graph, Literal, Namespace, URIRef
 from rdflib.namespace import DCTERMS, RDF, RDFS, SKOS, XSD
 import pandas as pd
 
-
+#
 input_file = csv.DictReader(open("magprof.csv"))
-
+#
 BK = Namespace("https://gams.uni-graz.at/o:depcha.bookkeeping#")
-
+GAMS = Namespace("https://gams.uni-graz.at/o:gams-ontology#")
+# 
 baseURL = "https://gams.uni-graz.at/"
-context = "context:marprof"
+CONTEXT = "context:marprof"
 PID = "o:marprof.1"
 
 # make a graph
 output_graph = Graph()
+# define namespace in output file
 output_graph.bind("bk", BK)
+output_graph.bind("gams", GAMS)
 
 # from is reserved term
 bk_from = URIRef("https://gams.uni-graz.at/o:depcha.bookkeeping#from")
@@ -22,28 +25,55 @@ bk_from = URIRef("https://gams.uni-graz.at/o:depcha.bookkeeping#from")
 for count, row in enumerate(input_file):
     # convert it from an OrderedDict to a regular dict
     row = dict(row)
-    
-    #TRANSACTION
+ 
+    # URIs
     Transaction = URIRef(baseURL + PID + "#T" + str(count))
     Transfer = URIRef(baseURL + PID + "#T" + str(count) + "T1")
-    Measurable = URIRef(baseURL + PID + "#T" + str(count) + "M1")
-    Between = URIRef(baseURL + PID + "#pers." + row['BK_BETWEENID'])
+    Measurable_Money1 = URIRef(baseURL + PID + "#T" + str(count) + "M1")
+    Measurable_Money2 = URIRef(baseURL + PID + "#T" + str(count) + "M2")
+    Measurable_Money3 = URIRef(baseURL + PID + "#T" + str(count) + "M3")
     
+    Between = URIRef(baseURL + PID + "#pers." + row['BK_BETWEENID'])
+    # Data
+    normalizedEntry = " ".join(row['BK_ENTRY'].split())
+    
+    #TRANSACTION
     output_graph.add((Transaction, RDF.type,  BK.Transaction))
     output_graph.add((Transaction, BK.when,  Literal(row['BK_WHEN']) ))
-    output_graph.add((Transaction, BK.entry,  Literal(row['BK_ENTRY']) ))
+    # normalize whitespace
+    output_graph.add((Transaction, BK.entry,  Literal(normalizedEntry) ))
     output_graph.add((Transaction, BK.consistsOf,  Transfer))
+    output_graph.add((Transaction, BK.type,  Literal(row['OBJECT_'])))
+    output_graph.add((Transaction, GAMS.isMemberOfCollection,  URIRef(baseURL + CONTEXT) ))
+    output_graph.add((Transaction, GAMS.isPartofTEI,  URIRef(baseURL + PID) ))
+    output_graph.add((Transaction, GAMS.textualContent,  Literal(normalizedEntry) ))
     
     #TRNSFER
     output_graph.add((Transfer, RDF.type,  BK.Transfer))
-    output_graph.add((Transfer, BK.transfers,  Measurable))
+    output_graph.add((Transfer, BK.transfers,  Measurable_Money1))
+    if(row['BK_MONEY2'] != "0.0"):
+        output_graph.add((Transfer, BK.transfers,  Measurable_Money2))
+    if(row['BK_MONEY3'] != "0.0"):
+        output_graph.add((Transfer, BK.transfers,  Measurable_Money3))
     output_graph.add((Transfer, bk_from, Between))
     output_graph.add((Transfer, BK.to,  URIRef(baseURL + PID + "#pers." + 'AGENT0') ))
     
-    #Measurable
-    output_graph.add((Measurable, RDF.type,  BK.Money))
-    output_graph.add((Transfer, BK.unit, Literal(row['BK_CURRENCY'])))
-    output_graph.add((Transfer, BK.quantity,  Literal(row['BK_MONEY1']) ))
+    #Measurable:Money1, Livre
+    output_graph.add((Measurable_Money1, RDF.type,  BK.Money))
+    output_graph.add((Measurable_Money1, BK.unit, Literal('Livre tournois') ))
+    output_graph.add((Measurable_Money1, BK.quantity,  Literal(row['BK_MONEY1']) ))
+    
+    #Measurable:Money2, Sous
+    if(row['BK_MONEY2'] != "0.0"):
+        output_graph.add((Measurable_Money2, RDF.type,  BK.Money))
+        output_graph.add((Measurable_Money2, BK.unit, Literal('Sous') ))
+        output_graph.add((Measurable_Money2, BK.quantity,  Literal(row['BK_MONEY2']) ))
+    
+    #Measurable:Money3, Deniers
+    if(row['BK_MONEY3'] != "0.0"): 
+        output_graph.add((Measurable_Money3, RDF.type,  BK.Money))
+        output_graph.add((Measurable_Money3, BK.unit, Literal('Deniers') ))
+        output_graph.add((Measurable_Money3, BK.quantity,  Literal(row['BK_MONEY3']) ))
     
     #BETWEEN
     output_graph.add((Between, RDF.type,  BK.Between))
